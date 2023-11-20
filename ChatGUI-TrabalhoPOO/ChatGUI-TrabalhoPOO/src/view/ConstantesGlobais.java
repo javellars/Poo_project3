eab5fb864bb9fe27a1d05cbb00531ebc97a3c052
Jullia S. Avelar <50809898+javellars@users.noreply.github.com>
2023-11-19 21:16:36 -0300

    Add files via upload
    Projeto de tela Sobre Finalizado

--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/ConstantesGlobais.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/ConstantesGlobais.java
@@ -1,21 +1,38 @@
 package src.view;
 
-public class ConstantesGlobais {
-    public static final String autor = "Jullia S.Avelar";
-    public static final String escola = "FT UNicamp - Faculdade de Tecnologia";
-    public static final String universidade = "Unicamp";
-    public static final String nome = "Interface Gráfica";
-    public static final String versao = "Ver. 1.0.0";
-    public static final String arqLogotipo = "imagem1L.jpg";
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class ConstantesGlobais {
+    public static final List<String> autor = new ArrayList<>();
+    public static final String escola = "FT- Faculdade de Tecnologia";
+    public static final String universidade = "Unicamp";
+    public static final String nome = "Interface Gráfica - créditos";
+    public static final String versao = "Version 1.0.2";
+    public static final String arqImg = "imgGP.png";
+
+    
+    static {
+        autor.add("Jullia Souza De Avelar 214539");
+        autor.add("Laís Azevedo Soares 173455");
+        autor.add("Victor Ferreira Da Silva 177950");
+        autor.add("Felipe Lemos Ferreira 174483");
+        autor.add("Clara Luiza De Andrade Klippel 234458");
+    }
+    
+    
+    
     static String getTextoSobre() {
         final StringBuilder text = new StringBuilder();
-
-        text.append(System.lineSeparator()).append(autor);
-        text.append(System.lineSeparator()).append(escola);
+       
         text.append(System.lineSeparator()).append(universidade);
-        text.append(System.lineSeparator()).append(nome);
+        text.append(System.lineSeparator()).append(escola);
+ //     text.append(System.lineSeparator()).append(nome);
         text.append(System.lineSeparator()).append(versao);
+        for (String autor : autor) {
+            text.append(System.lineSeparator()).append(autor);
+        }
 
         return text.toString();
     }
--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/PainelLogo.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/PainelLogo.java
@@ -5,46 +5,27 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.IOException;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class PainelLogo extends JPanel{
+public class PainelLogo extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	
-@Override
-public void paint (Graphics g) {
-	super.paint(g);
-	final Graphics2D auxGraphics = (Graphics2D)g;
-	
-	try {
-		final URL auxURL = this.getClass().getResource("/imagem1L.jpg");
-		final Image auxImage = ImageIO.read(auxURL);
-		auxGraphics.drawImage(auxImage,20,50,180,190,0,0,
-							auxImage.getWidth(null),
-							auxImage.getHeight(null),
-							null);
-		}
-	catch(final IOException e) {
-		System.out.println("Arquivo de imagem nao encontrado" + e.getMessage());
-	}
-	catch(Exception e) {
-		System.out.println("Erro ao carregar arquivo de imagem." + e.getMessage());
-	}
-		
-}
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    private static final long serialVersionUID = 1L;
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        final Graphics2D auxGraphics = (Graphics2D) g;
+
+        try {
+                final URL auxURL = this.getClass().getResource(ConstantesGlobais.arqImg);
+                final Image auxImage = ImageIO.read(auxURL);
+                auxGraphics.drawImage(auxImage, 20, 50, 180, 190, 0, 0, auxImage.getWidth(null), auxImage.getHeight(null), null);
+     
+        } catch (final IOException e) {
+            System.out.println("Erro ao carregar imagem: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar arquivo de imagem." + e.getMessage());
+        }
+    }
+}
