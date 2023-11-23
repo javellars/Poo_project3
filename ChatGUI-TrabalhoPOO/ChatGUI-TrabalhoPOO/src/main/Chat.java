a5537e35f2da71d30349e998777a4f36905d4f99
Claklippel <151850470+Claklippel@users.noreply.github.com>
2023-11-23 17:06:53 -0300

    Update Chat.java

--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/main/Chat.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/main/Chat.java
@@ -1,11 +1,14 @@
-package src.main;
-import src.view.Janela;
+package com.mycompany.projeto3poo;
 
+import com.mycompany.projeto3poo.src.view.Janela;
+/**
+ * A classe Chat é a classe principal do projeto que inicia a aplicação de chat.
+ */
 public class Chat {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+       // Cria uma instância da classe Janela, que representa a interface gráfica do chat.
         Janela janela = new Janela();
+        // Torna a janela visível para o usuário.
         janela.setVisible(true);
     }
 }
-
-                
