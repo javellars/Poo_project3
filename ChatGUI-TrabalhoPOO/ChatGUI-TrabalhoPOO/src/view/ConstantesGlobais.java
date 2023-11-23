020897a5db79dce0c5432d8d1afefa91a76d75cc
Claklippel <151850470+Claklippel@users.noreply.github.com>
2023-11-23 17:07:41 -0300

    Update ConstantesGlobais.java

--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/ConstantesGlobais.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/ConstantesGlobais.java
@@ -1,15 +1,36 @@
-package src.view;
+package com.mycompany.projeto3poo.src.view;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Esta classe contém constantes globais utilizadas na interface gráfica.
+ * Inclui informações sobre os autores, escola, universidade, nome, versão atual do programa e nome de arquivo de imagem.
+ */
 public class ConstantesGlobais {
+    /**
+     * Lista de autores que contribuíram para o trabalho.
+     */
     public static final List<String> autor = new ArrayList<>();
+    /**
+     * Nome do campus.
+     */
     public static final String escola = "FT- Faculdade de Tecnologia";
+    /**
+     * Nome da universidade.
+     */
     public static final String universidade = "Unicamp";
+        /**
+     * Nome da interface gráfica.
+     */
     public static final String nome = "Interface Gráfica - créditos";
+    /**
+     * Versão da interface gráfica.
+     */
     public static final String versao = "Version 1.0.2";
+    /**
+     * Nome do arquivo de imagem associado à interface gráfica.
+     */
     public static final String arqImg = "imgGP.png";
 
     
@@ -21,8 +42,11 @@ public class ConstantesGlobais {
         autor.add("Clara Luiza De Andrade Klippel 234458");
     }
     
-    
-    
+    /**
+     * Obtém o texto contendo informações sobre a universidade, escola, versão e autores.
+     *
+     * @return O texto com informações sobre a universidade, escola, versão e autores.
+     */
     static String getTextoSobre() {
         final StringBuilder text = new StringBuilder();
        
