ea8e53f7f406e389f050635cdf2c7489b2654408
Victor Ferreira da Silva <victor.ferreira.silva001@gmail.com>
2023-11-24 18:01:59 -0300

    conexao funcionando

--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/main/Chat.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/main/Chat.java
@@ -1,11 +1,92 @@
 package src.main;
+import src.view.ChatClient;
+import src.view.ChatServer;
 import src.view.Janela;
 
-public class Chat {
-    public static void main(String[] args) {
-        Janela janela = new Janela();
-        janela.setVisible(true);
-    }
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Chat{
+	public static void main(String[] args) {
+		int result = JOptionPane.showConfirmDialog(null,"Você será o Servidor do Chat? (Caso for conectar em algum outro sevidor, esolha não)", "Escolha",
+               JOptionPane.YES_NO_OPTION,
+               JOptionPane.QUESTION_MESSAGE);
+        if(result == JOptionPane.YES_OPTION){
+        	servidor();
+        }else if (result == JOptionPane.NO_OPTION){
+        	cliente();
+        }else {
+            System.exit(0);
+        }
+   }
+	
+	private static void servidor() {    
+      JFrame frame = new JFrame("Servidor");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JPanel panel = new JPanel();
+      LayoutManager layout = new FlowLayout();  
+      panel.setLayout(layout);    
+      final JLabel label = new JLabel();
+      label.setText("Esperando conexão do usuário...");
+      panel.add(label);
+      frame.getContentPane().add(panel, BorderLayout.CENTER);
+      frame.setSize(360, 100);      
+      frame.setLocationRelativeTo(null);  
+      frame.setVisible(true);
+      
+      ChatServer ser = new ChatServer(frame);
+      ser.setVisible(true);
+      ser.ReciveMessage();
+   }
+	
+	
+	
+	private static void cliente() {
+		JFrame frameC = new JFrame("Chat");
+		frameC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      JPanel panel = new JPanel();
+	      LayoutManager layout = new FlowLayout();  
+	      panel.setLayout(layout);    
+	      final JLabel label = new JLabel();
+	      label.setText("Digite o IP do Servidor: ");
+	      JTextField ipTextField = new JTextField("", 20);
+	      panel.add(label);
+	      panel.add(ipTextField, BorderLayout.CENTER);
+	      
+	      JButton button = new JButton("Conectar!");
+	      button.addActionListener(new ActionListener() {
+	    	  @Override
+	    	    public void actionPerformed(ActionEvent e) {
+	    	       String ip = ipTextField.getText();
+	    	       ChatClient c;
+	    			try {
+	    				c = new ChatClient(ip, frameC);
+	    	           c.setVisible(true);
+	    	           c.ReciveMessage();
+	    			} catch (IOException e1) {
+	    				// TODO Auto-generated catch block
+	    				e1.printStackTrace();
+	    			}
+	    	    }
+	      });
+
+	      panel.add(button);
+	      frameC.getContentPane().add(panel, BorderLayout.CENTER);
+	      frameC.setSize(360, 100);      
+	      frameC.setLocationRelativeTo(null);  
+	      frameC.setVisible(true);
+	}
 }
 
                 
---
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/ChatClient.java
@@ -0,0 +1,209 @@
+package src.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChatClient extends JFrame implements ActionListener {
+
+    // declarações
+    private JMenuBar menuBar;
+    private JMenu ajudaMenu;
+    private JMenu arquivoMenu;
+    private JMenuItem sobreItem;
+    private JMenuItem ajudaItem;
+    private JMenuItem conectarItem;
+    private JMenuItem sairItem;
+    private JButton enviarButton;
+    private JTextField mensagemTextField;
+    private JTextArea areaDeChat;
+    private JLabel statusConexaoLabel;
+    Socket s;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    // Construtor
+
+    public ChatClient(String ip, final JFrame frame) throws IOException {
+        setTitle("ChatGUI-Grupo 3");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 500);
+        setLayout(new BorderLayout());
+
+        menuSetup();
+        componentesSetup();
+        acaoSetup();
+        s = new Socket(ip, 9999); // We are going to use 9999 as the port number
+
+        out = new ObjectOutputStream(s.getOutputStream());
+        in = new ObjectInputStream(s.getInputStream());
+        frame.setVisible(false);
+    }
+    
+    public ChatClient() throws IOException {
+        setTitle("ChatGUI-Grupo 3");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 500);
+        setLayout(new BorderLayout());
+
+        menuSetup();
+        componentesSetup();
+        acaoSetup();
+        s = new Socket("127.0.0.1", 9999); // We are going to use 9999 as the port number
+
+        out = new ObjectOutputStream(s.getOutputStream());
+        in = new ObjectInputStream(s.getInputStream());
+    }
+    
+    private void mostrarTelaMensagem(TelaMensagem tela) {
+    	tela.setVisible(true);
+    }
+    
+    public void menuSetup(){
+     
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        arquivoMenu = new JMenu("Arquivo");
+        menuBar.add(arquivoMenu);
+        conectarItem = new JMenuItem("Conectar");
+        arquivoMenu.add(conectarItem);
+        sairItem = new JMenuItem("Sair");
+        arquivoMenu.add(sairItem);
+
+        ajudaMenu = new JMenu("Ajuda");
+        menuBar.add(ajudaMenu);
+        sobreItem = new JMenuItem("Sobre");
+        ajudaMenu.add(sobreItem);
+        ajudaItem = new JMenuItem("Ajuda");
+        ajudaMenu.add(ajudaItem);
+    }
+
+    public void componentesSetup(){
+        // Display chat
+        areaDeChat = new JTextArea();
+        areaDeChat.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(areaDeChat);
+        scrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(50, 50, 30, 50),BorderFactory.createLineBorder(Color.black)));
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Area sul(status+mensagem+botão))
+        JPanel areaSul = new JPanel();
+        areaSul.setLayout(new BorderLayout());
+        add(areaSul, BorderLayout.SOUTH);
+
+        JPanel areaMensagemPanel = new JPanel();
+        areaMensagemPanel.setLayout(new BorderLayout());
+        areaMensagemPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
+        areaSul.add(areaMensagemPanel, BorderLayout.CENTER);
+
+        // Campo de Texto
+        mensagemTextField = new JTextField();
+        areaMensagemPanel.add(mensagemTextField, BorderLayout.CENTER);
+
+        // Botao Enviar
+        enviarButton = new JButton("Enviar");
+        areaMensagemPanel.add(enviarButton, BorderLayout.EAST);
+
+        statusConexaoLabel = new JLabel("Status de conexão:");
+        statusConexaoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        statusConexaoLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+        areaSul.add(statusConexaoLabel, BorderLayout.SOUTH);
+    }
+
+    public void acaoSetup() {
+        // Eventos
+        enviarButton.addActionListener(this);
+        sobreItem.addActionListener(this);
+        ajudaItem.addActionListener(this);
+        conectarItem.addActionListener(this);
+        sairItem.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Ações
+        //JOptionOane.showMessageDialog(this, evento.getActionCommand(), "Ação Solicitada", JOptionPane.INFORMATION_MESSAGE);
+
+        if (e.getSource() == sobreItem) {
+            mostrarTelaMensagem(new TelaMensagem(this, "Sobre - " + ConstantesGlobais.versao, ConstantesGlobais.getTextoSobre()));
+            System.out.println("Clicou em sobre");
+        } else if (e.getSource() == ajudaItem) {
+            System.out.println("Clicou em ajuda");
+        } else if (e.getSource() == conectarItem) {
+            System.out.println("Clicou em conectar");
+        } else if (e.getSource() == sairItem) {
+            System.exit(0);
+        } else if (e.getSource() == enviarButton) {
+            /*String mensagem = mensagemTextField.getText();
+            if (!mensagem.isEmpty()) {
+                areaDeChat.append("Você: " + mensagem + "\n");
+                mensagemTextField.setText("");
+           }*/
+        	if (!mensagemTextField.getText().equals("")) { // TO be sure that our text box is not empty
+                String msg = mensagemTextField.getText();
+                SendMessage(msg); //We are going to create this function now
+                mensagemTextField.setText("");
+            }
+        }
+    }
+    
+    public static void main(String args[]) throws IOException {
+        ChatClient c = new ChatClient();
+        c.setVisible(true);
+        c.ReciveMessage();
+    }
+
+    private void SendMessage(String msg) {
+        try {
+            out.writeObject(msg);
+            areaDeChat.append("You: " + msg + "\n");
+        } catch (Exception e) {
+        }
+    }
+
+    public void ReciveMessage() {
+        String msg;
+        while (true) {
+            try {
+                msg = (String) in.readObject();
+                areaDeChat.append("Server: " + msg + "\n");
+            } catch (Exception e) {
+            }
+        }
+    }
+
+
+}
+
+
+/*
+ "Grupo:");
+"Laís Azevedo Soares 173455");
+"Jullia Souza De Avelar 214539");
+"Clara Luiza De Andrade Klippel 234458");
+"Victor Ferreira Da Silva 177950");
+"Felipe Lemos Ferreira 174483");
+ */
