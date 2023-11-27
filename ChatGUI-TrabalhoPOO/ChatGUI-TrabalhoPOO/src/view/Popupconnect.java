186fd577a61fddd855f9e2c6a7f6ed135e112a2f
FelpsLemos <74378235+FelpsLemos@users.noreply.github.com>
2023-11-27 10:18:53 -0300

    Update Popupconnect.java

--- ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/Popupconnect.java
+++ ChatGUI-TrabalhoPOO/ChatGUI-TrabalhoPOO/src/view/Popupconnect.java
@@ -1,19 +1,49 @@
-package src.view;
-
+package src.view
 import javax.swing.*;
 
-public class Popupconnect {
+public class PopUp {
 
-    public static String showPopUp() {
-        JTextField textField = new JTextField(10);
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Digite o valor de IP para conectar:"));
-        panel.add(textField);
+    public static class IPAndPort {
+        private String IPConnect;
+        private int PortaConnect;
 
-        int result = JOptionPane.showConfirmDialog(null, panel, "IPConnect", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            return textField.getText();
+        public IPAndPort(String IPConnect, int PortaConnect) {
+            this.IPConnect = IPConnect;
+            this.PortaConnect = PortaConnect;
         }
-        return null;
+
+        public String getIPConnect() {
+            return IPConnect;
+        }
+
+        public int getPortaConnect() {
+            return PortaConnect;
+        }
+    }
+
+    public static IPAndPort showPopUp() {
+        JTextField IPField = new JTextField(10);
+        JTextField portaField = new JTextField(10);
+
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Digite o valor de IP:"));
+        panel.add(IPField);
+        panel.add(Box.createHorizontalStrut(15)); // Add spacing
+        panel.add(new JLabel("Digite o valor da Porta:"));
+        panel.add(portaField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "IP e Porta", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            String IPConnect = IPField.getText();
+            int PortaConnect;
+            try {
+                PortaConnect = Integer.parseInt(portaField.getText());
+                return new IPAndPort(IPConnect, PortaConnect);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Por favor, insira um número válido para a porta.", "Erro", JOptionPane.ERROR_MESSAGE);
+                return showPopUp(); 
+            }
+        }
+        return null; 
     }
 }
